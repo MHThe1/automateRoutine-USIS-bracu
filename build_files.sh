a60435109176b25787b055e6d9e06950aad3d0f2
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-08-29 05:21:14 +0600

    more tries

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python3 manage.py collectstatic --noinput
