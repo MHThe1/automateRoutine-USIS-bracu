3565a3f529cfd41bf21857ccb4a3438f36cd12a9
Mehedi Hasan <mehedihtanvir@gmail.com>
2025-01-31 22:10:34 +0600

    allowed hosts updated

--- autoroutine/settings.py
+++ autoroutine/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.0.155", "localhost", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.0.155", "localhost", ".vercel.app", ".now.sh", ".onrender.com"]
 
 
 # Application definition
